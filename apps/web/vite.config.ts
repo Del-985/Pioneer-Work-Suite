e7a678e71e341c3d35ff72c14f3e180907943265
Del-985 <119140366+Del-985@users.noreply.github.com>
2025-12-02 07:40:55 -0600

    Update vite.config.ts

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  // base: "/Pioneer-Work-Suite/",
+  base: "/Pioneer-Work-Suite/", // 👈 important for GitHub Pages
 });
